8b490f48b34188fe02df0cedb95db39c1d35fa9e
Vignesh <vigneshkvs.p@gmail.com>
2017-03-01 07:58:53 +0530

    Login design added

--- app/src/main/java/com/mocktest/LoginActivity.java
+++ app/src/main/java/com/mocktest/LoginActivity.java
@@ -9,5 +9,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+/*        TextView _tv = (TextView) findViewById( R.id.textView1 );
+        new CountDownTimer(20*60000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                _tv.setText("seconds remaining: " +new SimpleDateFormat("mm:ss:SS").format(new Date( millisUntilFinished)));
+            }
+
+            public void onFinish() {
+                _tv.setText("done!");
+            }
+        }.start();*/
     }
 }
